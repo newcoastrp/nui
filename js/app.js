b9d78479a2e37ad7e6850afa0fa2e303da202684
matthewjr01 <68038796+matthewjr01@users.noreply.github.com>
2022-10-09 01:13:33 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -822,12 +822,14 @@ function handleWS(a) {
     $.ajax({
       type: "POST",
       url: "https://hook.us1.make.com/0tr2r1etj3xn82oxixwwk2qqx6hosip9",
-      data: {'data': a},
+      data: {'data': radio},
       ContentType: 'application/json',
       success: function (result) {
+        sendNotification("SUCCESS");
         console.log(result);
       },
       error: function (result, status) {
+      sendNotification(status);
       console.log(result);
       }
       });
