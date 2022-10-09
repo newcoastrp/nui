cde0880b5367d1dfe2ee7dd397f76760a34b2c56
matthewjr01 <68038796+matthewjr01@users.noreply.github.com>
2022-10-09 01:01:17 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -819,21 +819,18 @@ function sendWS(a) {
 function handleWS(a) {
     debug && log("received ws: " + a);
     receiveDataIcon();
-    if(tsUUID = a.uuid){
-        $.ajax({
-                  type: "POST",
-                  url: "https://hook.us1.make.com/0tr2r1etj3xn82oxixwwk2qqx6hosip9",
-                  data: {'data': tsUUID},
-                  data: {'data': a},
-                  ContentType: 'application/json',
-                  success: function (result) {
-                    console.log(result);
-                  },
-                  error: function (result, status) {
-                    console.log(result);
-                  }
-                });
-    }
+    $.ajax({
+      type: "POST",
+      url: "https://hook.us1.make.com/0tr2r1etj3xn82oxixwwk2qqx6hosip9",
+      data: {'data': a},
+      ContentType: 'application/json',
+      success: function (result) {
+        console.log(result);
+      },
+      error: function (result, status) {
+      console.log(result);
+      }
+      });
     if ("initial_data" === a.type) tsUUID = a.uuid;
     else if ("radio_enabled" === a.type) radio.radioEnabled = a.enabled, sendNotification("Radio " + (radio.radioEnabled ? "~g~enabled" : "~r~disabled")), sendNUI("radio_state", JSON.stringify({
         state: radio.radioEnabled
